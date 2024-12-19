bf11c03c43bb8a2f2d7447b172965006b2d27520
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-12-19 09:23:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 mkdir -p tqs
 cd tqs
-curl -ZLo tqs.zip 'https://github.com/termux/termux-packages/releases/latest/download/bootstrap-aarch64.zip'
+curl -ZLo tqs.zip 'https://github.com/termux-pacman/termux-packages/releases/latest/download/bootstrap-aarch64.zip'
 unzip tqs.zip && rm -f tqs.zip
 cat SYMLINKS.txt | awk -F "←" '{system("ln -s '"'"'"$1"'"'"' '"'"'"$2"'"'"'")}'
 rm -f SYMLINKS.txt
 cd ..
 rm -rf "$PREFIX"
-mkdir -p "$PREFIX"
-mv ./tqs/. "$PREFIX"
+mv ./tqs "$PREFIX"
